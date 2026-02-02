cf7bc2a567a9e3552ef2cec0b3930b91f586a01f
wjh1461 <wjh1461@naver.com>
2026-02-02 17:26:38 +0900

    에디터용 모듈 추가

---
+++ Plugins/AbilityCombatFramework/Source/AbilityCombatFrameworkEditor/Private/AbilityCombatFrameworkEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "AbilityCombatFrameworkEditor.h"
+
+#define LOCTEXT_NAMESPACE "FAbilityCombatFrameworkEditorModule"
+
+void FAbilityCombatFrameworkEditorModule::StartupModule()
+{
+    
+}
+
+void FAbilityCombatFrameworkEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FAbilityCombatFrameworkEditorModule, AbilityCombatFrameworkEditor)
