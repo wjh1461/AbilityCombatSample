b39efcad83828b28681f3797114c02d00cb261be
wjh1461 <wjh1461@naver.com>
2026-02-02 17:27:14 +0900

    플러그인 전용 로그 추가

---
+++ Plugins/AbilityCombatFramework/Source/AbilityCombatFramework/Public/CombatLog.h
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+ABILITYCOMBATFRAMEWORK_API DECLARE_LOG_CATEGORY_EXTERN(LogCombat, Log, All);
